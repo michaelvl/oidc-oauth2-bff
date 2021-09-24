f784a3f39ce262270f661673a224356124706507
MichaelVL <michael.larsen@eficode.com>
2021-09-24 06:43:22 +0200

    Add CORS support

--- bff/src/index.js
+++ bff/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const session = require('express-session');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const redis = require('redis')
 const randomstring = require("randomstring");
@@ -18,6 +19,7 @@ const oidc_token_url = process.env.OIDC_TOKEN_URL;
 const oidc_scope = process.env.OIDC_SCOPE || 'openid profile';
 const redis_url = process.env.REDIS_URL;
 const session_secret = process.env.SESSION_SECRET || randomstring.generate(32);
+const cors_allow_origin = process.env.CORS_ALLOW_ORIGIN;
 
 console.log('CLIENT_ID', client_id);
 console.log('CLIENT_SECRET', client_secret);
@@ -25,19 +27,27 @@ console.log('OIDC_AUTH_URL', oidc_auth_url);
 console.log('OIDC_TOKEN_URL', oidc_token_url);
 console.log('OIDC_SCOPE', oidc_scope);
 console.log('REDIS_URL', redis_url);
+console.log('CORS_ALLOW_ORIGIN', cors_allow_origin);
 
 const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+if (cors_allow_origin) {
+    app.use(cors({
+        origin: cors_allow_origin,
+        credentials: true
+    }));
+}
+
 const session_config = { secret: session_secret,
                          resave: false,
                          saveUninitialized: false,
-			 cookie: {
-			     httpOnly: true,
-			     sameSite: 'strict'
-			 }
-		       };
+                         cookie: {
+                             httpOnly: true,
+                             sameSite: 'strict'
+                         }
+                       };
 if (app.get('env') === 'production') {
     app.set('trust proxy', 1)
     session_config.cookie.secure = true
@@ -84,65 +94,77 @@ app.post('/continue', (req, res) => {
     let idp_state = data.state
 
     if (code && req.session.state == idp_state) {
-	console.log('Login continuation using code', code, 'and state', idp_state);
-	// This is a confidential client - authorize towards IdP with client id and secret
-	const client_creds = 'Basic ' + Buffer.from(querystring.escape(client_id)+':'+querystring.escape(client_secret), 'ascii').toString('base64')
-	const data = querystring.encode({
+        console.log('Login continuation using code', code, 'and state', idp_state);
+        // This is a confidential client - authorize towards IdP with client id and secret
+        const client_creds = 'Basic ' + Buffer.from(querystring.escape(client_id)+':'+querystring.escape(client_secret), 'ascii').toString('base64')
+        const data = querystring.encode({
             code: code,
             grant_type: 'authorization_code',
             redirect_uri: redir_url});
-	const options = {
+        const options = {
             method: 'POST',
             headers: {
-		'Authorization': client_creds,
-		'Content-Type': 'application/x-www-form-urlencoded',
-		'Content-Length': data.length
+                'Authorization': client_creds,
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Length': data.length
             }
-	};
+        };
 
-	// Exchange code for tokens using the token endpoint
-	const post = https.request(oidc_token_url, options, (post_resp) => {
-            console.log('statusCode:', post_resp.statusCode);
-            post_resp.on('data', (data) => {
-		const token_data = JSON.parse(data);
-		console.log('Token response', token_data);
-		if (post_resp.statusCode == 200){
+        // Exchange code for tokens using the token endpoint
+        const post = https.request(oidc_token_url, options, (post_resp) => {
+            if (post_resp.statusCode != 200) {
+                console.log('statusCode:', post_resp.statusCode);
+                res.status(500).send();
+            } else {
+                post_resp.on('data', (data) => {
+                    const token_data = JSON.parse(data);
+                    console.log('Token response', token_data);
                     if (token_data.id_token) {
-			req.session.id_token =  token_data.id_token;
-			console.log('ID token', req.session.id_token);
+                        req.session.id_token =  token_data.id_token;
+                        console.log('ID token', req.session.id_token);
 
-			// TODO: Validate signature on id_token
+                        // TODO: Validate signature on id_token
 
-			req.session.id_token_claims = jwt_decode(req.session.id_token);
-			console.log('ID token claims', req.session.id_token_claims);
+                        req.session.id_token_claims = jwt_decode(req.session.id_token);
+                        console.log('ID token claims', req.session.id_token_claims);
                     }
                     if (token_data.access_token) {
-			req.session.access_token = token_data.access_token
-			console.log('Access token', req.session.access_token);
+                        req.session.access_token = token_data.access_token
+                        console.log('Access token', req.session.access_token);
                     }               
                     if (token_data.refresh_token) {
-			req.session.refresh_token = token_data.refresh_token
-			console.log('Refresh token', req.session.refresh_token);
-                    }               
-		    res.status(200).json({loggedIn: true});
-		}
-            });
-	});
-	post.write(data);
-	post.end();
+                        req.session.refresh_token = token_data.refresh_token
+                        console.log('Refresh token', req.session.refresh_token);
+                    }
+                    res.status(200).json({loggedIn: true});
+                });
+            }
+        });
+        post.write(data);
+        post.end();
     } else {
-        console.log('Error, code missing or state mismatch.');
+        if (!code) {
+            console.log('Error, code missing.');
+        }
+        if (req.session.state != idp_state) {
+            console.log('Error, state mismatch.', req.session.state, 'vs', idp_state);
+        }
         console.log('req.body', req.body);
         console.log('req.session', req.session);
-        return res.status(403);
+        return res.status(200).json({loggedIn: false});
     }
 
 });
 
 app.get('/userinfo', (req, res) => {
     console.log('GET', req.originalUrl)
-    console.log('ID token claims', req.session.id_token_claims);
-    res.status(200).json(req.session.id_token_claims);
+    if (req.session.id_token_claims) {
+        console.log('ID token claims', req.session.id_token_claims);
+        res.status(200).json(req.session.id_token_claims);
+    } else {
+        console.log('*** No ID token claims');
+        res.status(200).json({});
+    }
 });
 
 app.listen(port, () => {
